ac97d3d11113a46e2884a00f638914e95fc0ae3b
i am DCJ <clarkjd.j@gmail.com>
2016-05-27 02:18:10 +0000

    Dirty but passes

--- linked_list/main.py
+++ linked_list/main.py
@@ -44,16 +44,20 @@ class Node(object):
     """
 
     def __init__(self, elem, next=None):
-        pass
+        self.elem = elem
+        self.next = next
 
     def __str__(self):
-        pass
+        return str(self.elem)
 
     def __eq__(self, other):
-        pass
+        if isinstance(other, LinkedList):
+            return self.elem == other.elem #compare node.elem to value passed to it
+        else:
+            return self.elem == other
 
     def __repr__(self):
-        pass
+        return "<Node {}>".format(self.elem)
 
 
 class LinkedList(AbstractLinkedList):
@@ -62,34 +66,108 @@ class LinkedList(AbstractLinkedList):
     """
 
     def __init__(self, elements=None):
-        pass
+        # start at none
+        self.start = None
+        self.end = None
+        #[1,2], self.start (Node(1)) >> Node1.next now pts to Node(2) 
+        # set elements to empty list
+        elements = [] if not elements else elements
+        for val in elements:
+            self.append(val)
+            
 
     def __str__(self):
-        pass
-
+        return str([element.elem for element in self])
+    
     def __len__(self):
-        pass
+        return self.count()
 
     def __iter__(self):
-        pass
+        n = self.start
+        while n:
+            yield n
+            n = n.next
 
     def __getitem__(self, index):
-        pass
+       for n, node in enumerate(self): 
+           if n == index:
+               return node
 
     def __add__(self, other):
-        pass
+       l1 = [ n.elem for n in self]
+       l2 = [ n.elem for n in other]
+       l3 = l1+l2
+       return LinkedList(l3)
 
     def __iadd__(self, other):
-        pass
+        #self = self + other
+        return self.__add__(other) #  x += 1  == x = x+1
+       # for i in other.__iter__():
+        #    self.append(i.elem)
+        #return self    
 
     def __eq__(self, other):
-        pass
+        return [element.elem for element in self] == [element.elem for element in other]
 
     def append(self, elem):
-        pass
-
+        #new node
+        if self.start == None:  #is it the first node
+            self.start = Node(elem)
+            self.end = Node(elem)
+        elif self.start == self.end:
+            self.start.next = Node(elem) #fix first node when adding 2nd
+            self.end.next = self.start.next #prior node now  points to new node
+            self.end = self.end.next #new node is now end
+        else:
+            self.end.next = Node(elem) #prior node now  points to new node
+            self.end = self.end.next #new node is now end
+                
+        
     def count(self):
-        pass
+        c = 0
+        for e in self:
+            c +=1
+        return c
+        
 
     def pop(self, index=None):
-        pass
+        prior = self.start
+        if index >= len(self)  or len(self)== 0:
+            raise IndexError()
+        elif len(self) == 1:
+            temp = self.end
+            self.start, self.end = None, None
+            return temp
+        elif index == None:
+            for element in self:
+                if element.next == None:
+                    prior.next = None
+                    self.end = prior.next
+                    return element
+                else:
+                    prior = element
+        else:
+            if (not self[index]) or (len(self) < index):  #pop(empty), pop(too big), 
+                raise IndexError
+            if self[index].next == None:
+                temp = self[index]
+                if self[index-1]:
+                    self[index-1].next = None
+                    self.end = self[index-1]
+                    return temp
+                else:
+                    self.start = self[index].next
+                    return temp
+            else:    
+                temp2 = self[index]
+                if self[index-1]:
+                    self[index-1].next = self[index+1]
+                    return temp2
+                else: 
+                    self.start = self[index].next
+                    return temp2
+
+
+
+
+
